7816d681cb3fde24a6aeff0b060ba640fe83124f
krittikatiwari <113347197+krittikatiwari@users.noreply.github.com>
2022-12-26 15:03:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Components from './components/components';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Components/>
+    </div>
+  );
+}
+
+export default App;
